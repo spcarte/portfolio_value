b41923b2a006740b7b25e269a392d6a8f0e21dfb
Steven Carter <spcarter@mtu.edu>
2025-01-18 21:42:32 -0700

    Added time value interpolation function to date_utilities

--- src/portfolio_value/date_utilities.py
+++ src/portfolio_value/date_utilities.py
@@ -5,6 +5,7 @@ objects.
 
 import datetime as dt
 import holidays
+import numpy as np
 
 def is_weekday(date):
   return date.weekday() < 5  # Monday to Friday
@@ -30,3 +31,25 @@ def business_day_vector_to_target(start_date, end_date):
             business_days.append(current_date)
         current_date += one_day
     return business_days
+
+def interpolate_time_value(original_dates, original_value, want_dates):
+    """
+    Interpolates the given values from the original dates to the desired dates.
+
+    Parameters
+    ----------
+    original_dates : datetime
+        A list of datetime values that correspond to the original values.
+    original_values : ndarray
+        A 1d array of values that correspond to the original dates. 
+    want_dates : datetime
+        A list of datetime values to interpolate the original values to.
+
+    Returns 
+    -------    
+    want_values : ndarray
+        A 1d array of values that correspond to the want_dates
+    """
+    original_dates_between = np.cumsum(np.insert(np.array([(original_dates[ii+1]-original_dates[ii]).days for ii in range(len(original_dates)-1)]), 0, 0))
+    want_dates_between = np.cumsum(np.insert(np.array([(want_dates[ii+1]-want_dates[ii]).days for ii in range(len(want_dates)-1)]), 0, 0))
+    return np.interp(want_dates_between, original_dates_between, original_value)
--- src/portfolio_value/portfolio_value.py
+++ src/portfolio_value/portfolio_value.py
@@ -24,3 +24,23 @@ class PortfolioValue:
     expected_future_fund_value : ndarray
         The expected (i.e., the mean) future time-value of the fund.
     """
+    def __init__(self, historical_data, initial_value=0):
+        """
+        Initializes the FundValue object
+
+        Parameters
+        ----------
+        historical_data : DataFrame
+            A Pandas DataFrame with the historical data for the stocks in
+            the portfolio (with a separate column for each stock), which 
+            will be the basis for the prediction of the future value. The
+            columns should be labeled with the stock ticker for each stock
+            in the fund. 
+        fund_allocation : DataFrame
+            A Pandas DataFrame with the fund allocation (through the 
+            target date for the portfolio), where The columns are labeled 
+            with the stock ticker for each stock in the fund. 
+        initial_value : float
+            The initial amount that is invested in the portfolio (the sum 
+            for all the stocks).  
+        """   
