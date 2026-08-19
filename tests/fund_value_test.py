68475570538f4a698552e3e041514195766c9a98
Steven Carter <spcarter@mtu.edu>
2026-08-18 18:19:14 -0600

    Added a test to make sure that the FundValue object computes the correct stats from historical data.

--- tests/fund_value_test.py
+++ tests/fund_value_test.py
@@ -8,6 +8,16 @@ import portfolio_value as pv
 import pandas as pd
 import pytest
 
+def work_days(start_date, number_days):
+    holiday_calendar = holidays.country_holidays('US')
+    workdays = [start_date]
+    current_date = start_date + dt.timedelta(days=1)
+    while len(workdays) < number_days:
+        if current_date.weekday() < 5 and current_date not in holiday_calendar:
+            workdays.append(current_date)
+        current_date += dt.timedelta(days=1)
+    return workdays
+
 @pytest.fixture()
 def simulated_data():
     """
@@ -27,18 +37,36 @@ def simulated_data():
     for ii in range(1,number_samples):
         simulated_stock[ii] += simulated_stock[ii-1]*test_path[ii]
 
-    holiday_calendar = holidays.country_holidays('US')
-    def work_days(start_date, number_days):
-        workdays = [start_date]
-        current_date = start_date + dt.timedelta(days=1)
-        while len(workdays) < number_days:
-            if current_date.weekday() < 5 and current_date not in holiday_calendar:
-                workdays.append(current_date)
-            current_date += dt.timedelta(days=1)
-        return workdays
-
     index_days = work_days(dt.datetime(2026,8,3), number_samples)
     return pd.DataFrame({'test':simulated_stock}, index=index_days)
+
+@pytest.fixture()
+def simulated_data_known_stats():
+    """
+    Creates sample data for a single "stock" with known statistical properties.
+    """
+    seed = 400
+    single_rng = np.random.default_rng(seed)
+
+    std=0.01
+    average=0.005
+    number_samples=500
+
+    shocks = single_rng.normal(loc=0, scale=1, size=number_samples-1)
+    shocks = shocks*std/shocks.std()
+    shocks = shocks - shocks.mean()
+
+    sample_data = np.zeros(number_samples, dtype=float)
+    sample_data[0] = 100
+    for ii in range(1,number_samples):
+        # Don't use the actual GBM formulation to make sure the simulated data 
+        # has the exact stats
+        sample_data[ii] = sample_data[ii-1]*np.exp(average+shocks[ii-1])
+
+    index_days = work_days(dt.datetime(2026,8,3), number_samples)
+    return pd.DataFrame({'test':sample_data}, index=index_days)
+
+# Want to check for normality on the predicted log returns
 
 def test_fund_value_expectation(simulated_data):
     """
@@ -62,3 +90,21 @@ def test_fund_value_expectation(simulated_data):
     max_error_from_expectation = np.max((predicted_expectation-simulated_stock_no_volatility)/predicted_expectation)*100
 
     assert max_error_from_expectation < 0.5
+
+def test_fund_value_stats(simulated_data_known_stats):
+    """
+    Tests that the fund value computes some known statistical properties 
+    from a set of sample data
+    """
+    # should match the stats in the sample stock data
+    std=0.01
+    average=0.005
+    drift = average + 0.5*(std**2)
+
+    simulated_stock = simulated_data_known_stats
+    stock_fv = pv.FundValue(simulated_stock['test'], initial_value=100)
+
+    assert stock_fv._initial_value_ == 100
+    assert np.isclose(stock_fv._log_drift_, average)
+    assert np.isclose(stock_fv._volatility_, std)
+    assert np.isclose(stock_fv._drift_, drift)
